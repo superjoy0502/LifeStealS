7e4080946687879ada1078f9fb8f73174c329a29
SuperJoy <superjoy0502@gmail.com>
2021-12-31 21:52:58 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    kotlin("jvm") version "1.6.0"
+    kotlin("plugin.serialization") version "1.6.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven(url = "https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+
+    implementation(kotlin("stdlib"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.mockito:mockito-core:4.1.0")
+}
+
+project.extra.set("packageName", name.replace("-", ""))
+project.extra.set("pluginName", name.split('-').joinToString("") { it.capitalize() })
+
+tasks {
+    processResources {
+        filesMatching("**/*.yml") {
+            expand(project.properties)
+            expand(project.extra.properties)
+        }
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    create<Jar>("paperJar") {
+        from(sourceSets["main"].output)
+        archiveBaseName.set(project.extra.properties["pluginName"].toString())
+        archiveVersion.set("") // For bukkit plugin update
+
+        doLast {
+            copy {
+                from(archiveFile)
+                val plugins = File(rootDir, ".debug/plugins/")
+                into(if (File(plugins, archiveFileName.get()).exists()) File(plugins, "update") else plugins)
+            }
+        }
+    }
+
+    shadowJar {
+        from(sourceSets["main"].output)
+        archiveBaseName.set(project.extra.properties["pluginName"].toString())
+        archiveVersion.set("") // For bukkit plugin update
+
+        doLast {
+            copy {
+                from(archiveFile)
+                val plugins = File(rootDir, ".debug/plugins/")
+                into(if (File(plugins, archiveFileName.get()).exists()) File(plugins, "update") else plugins)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/io/github/monun/sample/plugin/SamplePlugin.kt
@@ -0,0 +1,11 @@
+package io.github.monun.sample.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+/**
+ * @author Monun
+ */
+class SamplePlugin : JavaPlugin() {
+    override fun onEnable() {
+    }
+}
