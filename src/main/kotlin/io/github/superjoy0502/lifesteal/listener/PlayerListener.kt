bdbcee93ae46d052e9cef1ece63661b26436f4f0
SuperJoy <superjoy0502@gmail.com>
2022-01-02 22:12:17 -0700

    두번째 커밋

--- src/main/kotlin/io/github/superjoy0502/lifesteal/listener/PlayerListener.kt
+++ src/main/kotlin/io/github/superjoy0502/lifesteal/listener/PlayerListener.kt
@@ -6,33 +6,56 @@ import org.bukkit.attribute.Attribute
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerQuitEvent
 
 class PlayerListener(val plugin: LifeStealPlugin) : Listener {
 
     @EventHandler
     fun playerKilledEvent(event: PlayerDeathEvent) {
 
-        if (!plugin.started) return
+        if (!plugin.started) return // 게임 시작했는지 확인
 
         val victim = event.player
-        val killer = victim.killer ?: return
-        if (victim == killer) return
+        val killer = victim.killer ?: return // 플레이어가 죽인건지 확인
+        if (victim == killer) return // 자살인지 확인
 
-        val victimMaxHealth = victim.getAttribute(Attribute.GENERIC_MAX_HEALTH)
-
-        if (victimMaxHealth?.baseValue == 2.0) {
+        if (victim.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue == 2.0) { // 더 이상 깎을 체력이 없을 때
 
             victim.gameMode = GameMode.SPECTATOR
+            plugin.survivorList.remove(victim)
+
+            if (plugin.survivorList.size == 1) {
+
+                plugin.endGame()
+
+            }
+
             return
 
         }
 
-        val killerMaxHealth = killer.getAttribute(Attribute.GENERIC_MAX_HEALTH)
-
-        victimMaxHealth?.baseValue = victimMaxHealth?.baseValue?.minus(2)!!
-        killerMaxHealth?.baseValue = killerMaxHealth?.baseValue?.plus(2)!!
-
+        victim.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue =
+            victim.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue?.minus(2)!!
+        killer.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue =
+            killer.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue?.plus(2)!!
 
     }
+
+    /*@EventHandler
+    fun playerDisconnectEvent(event: PlayerQuitEvent) {
+
+        if (!plugin.participantList.contains(event.player)) return
+
+        event.player.getAttribute(Attribute.GENERIC_MAX_HEALTH)?.baseValue = 20.0
+
+    }
+
+    @EventHandler
+    fun playerJoinEvent(event: PlayerJoinEvent) {
+
+
+
+    }*/
 
 }
--- src/main/kotlin/io/github/superjoy0502/lifesteal/plugin/LifeStealPlugin.kt
+++ src/main/kotlin/io/github/superjoy0502/lifesteal/plugin/LifeStealPlugin.kt
@@ -2,8 +2,14 @@ package io.github.superjoy0502.lifesteal.plugin
 
 import io.github.monun.kommand.kommand
 import io.github.superjoy0502.lifesteal.listener.PlayerListener
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.title.Title
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
+import org.bukkit.GameMode
+import org.bukkit.Location
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
@@ -12,11 +18,15 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class LifeStealPlugin : JavaPlugin() {
 
-    val pluginVersion = "0.1.0"
-    val lifesteal = "${ChatColor.RED}LifeSteal${ChatColor.RESET}"
+    private val pluginVersion = "0.1.0b"
+    private val lifesteal = "${ChatColor.RED}LifeSteal${ChatColor.RESET}"
+    private val playerListener = PlayerListener(this)
 
-    val playerListener = PlayerListener(this)
     var started = false
+    var initialized = false
+    var participantList: ArrayList<Player> = ArrayList()
+    var survivorList: ArrayList<Player> = ArrayList()
+    var centreLocation: Location? = null
 
     override fun onEnable() {
 
@@ -34,11 +44,36 @@ class LifeStealPlugin : JavaPlugin() {
                     player.sendMessage(
                         """
                             $lifesteal 플러그인 v$pluginVersion
-                            ==========도움말==========
+                            ====================도움말====================
                             /lifesteal - 이 도움말을 출력합니다
+                            /lifesteal init - $lifesteal 게임의 중심 위치을 정합니다 (시작 전 필수)
                             /lifesteal start - $lifesteal 게임을 시작합니다
                         """.trimIndent()
                     )
+
+                }
+
+                then("init") {
+
+                    executes {
+
+                        if (started) {
+
+                            player.sendMessage(
+                                "${ChatColor.RED}이미 게임은 시작되었습니다${ChatColor.RESET}" +
+                                        "게임은 최후의 1인이 남을때까지 계속됩니다"
+                            )
+                            return@executes
+
+                        }
+
+                        centreLocation = player.location
+                        player.sendMessage(
+                            "$lifesteal 게임의 중심이 $centreLocation 으로 정해졌습니다"
+                        )
+                        initialized = true
+
+                    }
 
                 }
 
@@ -46,11 +81,59 @@ class LifeStealPlugin : JavaPlugin() {
 
                     executes {
 
-                        if (started) player.sendMessage(
-                            "${ChatColor.RED}이미 게임은 시작되었습니다${ChatColor.RESET}" +
-                                    "게임은 최후의 1인이 남을때까지 계속됩니다"
-                        )
+                        if (!initialized) {
+
+                            player.sendMessage("${ChatColor.RED}/lifesteal init 으로 게임 초기 설정을 먼저 해주세요!")
+                            return@executes
+
+                        }
+
+                        if (started) {
+
+                            player.sendMessage(
+                                "${ChatColor.RED}이미 게임은 시작되었습니다${ChatColor.RESET}" +
+                                        "게임은 최후의 1인이 남을때까지 계속됩니다"
+                            )
+                            return@executes
+
+                        }
+
+                        if (centreLocation == null) {
+
+                            centreLocation = Location(
+                                player.world,
+                                0.0,
+                                player.world.getHighestBlockAt(0, 0).y.toDouble(),
+                                0.0
+                            )
+
+                        }
+
                         started = true
+                        participantList = ArrayList(server.onlinePlayers)
+                        survivorList = participantList
+                        for (player in participantList) {
+
+                            player.gameMode = GameMode.SURVIVAL
+
+                        }
+
+                    }
+
+                }
+
+                then("debug") {
+
+                    executes {
+
+                        if (!initialized) {
+
+                            player.sendMessage("초기 설정 안함")
+                            return@executes
+
+                        }
+
+                        player.sendMessage("초기 설정 함")
 
                     }
 
@@ -59,6 +142,27 @@ class LifeStealPlugin : JavaPlugin() {
             }
 
         }
+
+    }
+
+    fun reset() {
+
+
+
+    }
+
+    fun endGame() {
+
+        for (player in server.onlinePlayers) {
+
+            player.showTitle(Title.title(
+                Component.text("${ChatColor.GREEN}${survivorList[0].name}님 우승!"),
+                Component.text(
+                    "${ChatColor.RED}Max HP: ${survivorList[0].getAttribute(Attribute.GENERIC_MAX_HEALTH)}")))
+
+        }
+
+        reset()
 
     }
 
